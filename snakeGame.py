4b903cc156521662c65ee3dc1ec5a8b279ebb3c7
Tanmay Hemant Karadkar <karadkarth@gmail.com>
2018-03-16 11:15:58 +0530

    Initial commit

---
+++ snakeGame.py
@@ -0,0 +1,17 @@
+#Tanmay H. Karadkar <karadkarth@gmail.com>
+#Snake Game
+#This project is from the free Udacity course to develop a pygame. 
+#Link: https://www.udemy.com/python-game-development-creating-a-snake-game-from-scratch
+#Note: I'm developing this game just for the sake of getting an experience in pygame
+#Feel free to download it, and modify it the way you want.
+
+
+import pygame                       #straighforward importing of pygame
+import sys          
+import random                       #because the random function is most useful in many games
+import time                         #to keep track of certain time units 
+
+#Here, import can also be written as import pygame, sys, random, time
+#I just wanted to explain the purpose of each import statement
+
+print(pygame.init())                #initializing pygame
